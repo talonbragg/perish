76b095d503426e2342e6384b6003e64cebfe8a45
talonbragg <pvpgamecrash@gmail.com>
2018-06-19 10:20:52 -0700

    changing what the game is about

--- js/main.js
+++ js/main.js
@@ -1,39 +1,71 @@
-var respBox = document.getElementById("responsebox");
-var messages = document.getElementById("messages");
-var startMenuUsed = false;
-var inventoryItems = [];
+/*
+  Main progress percentage
+  This will go up 1% or +1 each time the Game() function runs
+*/
+var progress = 0;
 
-function submit() {
-  if(respBox.value === "start") {
-    var node1 = document.createElement("LI");
-    var node2 = document.createElement("LI");
-    var node3 = document.createElement("LI");
-    var node4 = document.createElement("LI");
-    var starttext = document.createTextNode("You start in the kitchen, in there you find a knife and bread.");         // Create a text node
-    var startoption1 = document.createTextNode("1. Pick up the knife and bread");
-    var startoption2 = document.createTextNode("2. Eat the bread and pick up the knife");
-    var startoption3 = document.createTextNode("3. Go to the next room");
-    node1.appendChild(starttext);
-    node2.appendChild(startoption1);
-    node3.appendChild(startoption2);
-    node4.appendChild(startoption3);                              // Append the text to <li>
-    document.getElementById("messages").appendChild(node1);
-    document.getElementById("messages").appendChild(node2);
-    document.getElementById("messages").appendChild(node3);
-    document.getElementById("messages").appendChild(node4);
-    startMenuUsed = true;
-  }
-  if(startMenuUsed === true && respBox.value === "ok") {
-    return;
-  }
-  if(respBox.value === "1") {
-    var bknode = document.createElement("LI");
-    var bktext = document.createTextNode("The bread and knife are now in your inventory.");
-    inventoryItems.push("bread");
-    inventoryItems.push("knife");
-    bknode.appendChild(bktext);
-    messages.appendChild(bknode);
-  }
+/*
+  Universal human health
+*/
+
+var uniHealth = 100
+
+/*
+  This area will contain all of the data for the player
+*/
+
+var playerHealth = 100;
+var playerDamage = 50;
+
+/*
+  This area will contain all o the enemy data
+*/
+
+var enemyInfantryDamage = 35;
+var enemyMedicDamage = 5;
+var enemyTankDamage = 70;
+
+/* 
+  Tank health
+*/
+
+var enemyTankHealth = 200;
+
+//Main area to append a <li> element
+var messages = document.getElementById("messages");
+
+//Situation array
+var situations = ["Nothing has happened", "Nothing has happened", "Nothing has happened", "Nothing has happened", "An enemy is standing in front of you.", "You got shot in the Leg!", "You are gettting shelled my enemy forces", "You are hungry"]
+//Main game function
+function Game() {
+  console.log('Main Game function has initiated');
+
+  console.log(situations[Math.floor(Math.random() * situations.length)]);
+  console.log(typeof situations[Math.floor(Math.random * situations.length - 1)]);
+
+  var randSituation = situations[Math.floor(Math.random * situations.length - 1)];
+
+  var node1 = document.createElement("LI");
+  var node2 = document.createElement("LI");
+  var node3 = document.createElement("LI");
+  var node4 = document.createElement("LI");
+  var node5 = document.createElement("LI");
+  var situation = document.createTextNode(randSituation);         // Create a text node
+  var startoption1 = document.createTextNode("1. Shoot your rifle");
+  var startoption2 = document.createTextNode("2. Call A Medic");
+  var startoption3 = document.createTextNode("3. Dig a ditch and hide");
+  var startoption4 = document.createTextNode("4. Keep running towards the enemy fort");
+  node1.appendChild(situation);
+  node2.appendChild(startoption1);
+  node3.appendChild(startoption2);
+  node4.appendChild(startoption3);
+  node5.appendChild(startoption4);                              // Append the text to <li>
+  messages.appendChild(node1);
+  messages.appendChild(node2);
+  messages.appendChild(node3);
+  messages.appendChild(node4);
+  messages.appendChild(node5);
 }
 
-
+//Run the Game() function
+Game();
